0727fd4e2a7e132444c6bf5ac5fae2f6ca0a0c49
Kirsty Burgoine <info@kirstyburgoine.co.uk>
2016-08-29 14:09:56 +0100

    Updated header to include title

--- wp-content/themes/glowline/header.php
+++ wp-content/themes/glowline/header.php
@@ -7,10 +7,11 @@
 <html <?php language_attributes(); ?> >
 	<head>
 		<meta charset="<?php bloginfo('charset'); ?>" />
-		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title><?php wp_title( '|', true, 'right' ); ?></title>
 		<link rel="profile" href="http://gmpg.org/xfn/11" />
 		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
-		<?php    wp_head();   ?>
+		<?php wp_head(); ?>
 	</head>
 	<body <?php body_class(); ?>>
 	<!--Main Header Start -->
