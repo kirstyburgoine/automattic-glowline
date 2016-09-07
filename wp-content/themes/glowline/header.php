4b42687ee96b0a592d2e99f118957d0e257dc84e
Kirsty Burgoine <info@kirstyburgoine.co.uk>
2016-09-07 19:10:44 +0100

    Updated header settings to not show strapline

--- wp-content/themes/glowline/header.php
+++ wp-content/themes/glowline/header.php
@@ -23,12 +23,12 @@
 					<!-- Logo Start -->
 					<div class="logo">
 						<?php glowline_the_custom_logo(); ?>
-						<?php
+						<?php /*
 						if ( is_front_page() && is_home() ) : ?>
 							<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 						<?php else : ?>
 							<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-						<?php endif; ?>
+						<?php endif; */ ?>
 					</div>
 					<!-- Menu Start -->
 					<div id="main-menu-wrapper">
