0e99aa103df7590bbf686b1bad16629229144869
Kirsty Burgoine <info@kirstyburgoine.co.uk>
2016-09-10 13:12:23 +0100

    Removed custom social from slider

--- wp-content/themes/glowline/template/main-slider.php
+++ wp-content/themes/glowline/template/main-slider.php
@@ -32,15 +32,7 @@ $i = 0;
                     <p>  <?php echo glowline_get_custom_excerpt(); ?> </p>
                     <div class="read-more read-more-slider"><a href="<?php the_permalink(); ?>"><?php _e('Continue Reading...','glowline'); ?></a></div>
                     <div class="slider-post-meta">
-                        <ul class="slider-post-social social-icon">
-                            <li class="share-text"><?php _e('Share:','glowline' ); ?></li>
-                            <li><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=<?php the_permalink(); ?>"><i class="fa fa-facebook"></i></a></li>
-                            <li><a target="_blank" href="https://plus.google.com/share?url=<?php the_permalink(); ?>"><i class="fa fa-google-plus"></i></a></li>
-                            <li><a target="_blank" href="https://www.linkedin.com/shareArticle?mini=true&url=<?php the_permalink(); ?>&title=<?php the_title(); ?>&source=LinkedIn"><i class="fa fa-linkedin"></i></a></li>
-                            <li><a target="_blank" href="https://twitter.com/home?status=<?php the_title(); ?>-<?php the_permalink(); ?>"><i class="fa fa-twitter"></i></a></li>
-                            <li><a target="_blank" data-pin-do="skipLink"  href="https://pinterest.com/pin/create/button/?url=<?php the_permalink(); ?>&amp;media=&amp;description=<?php the_title(); ?>"><i class="fa fa-pinterest"></i></a></li>
-                            <li><span class="slider-post-comment"><?php comments_popup_link('', '<i class="fa fa-comments-o"></i>', '<i class="fa fa-comments-o"></i>'); ?></span></li>
-                        </ul>
+
                     </div>
                 </div>
             </div>
