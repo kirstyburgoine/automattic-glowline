4c337c61ec719373b692877b6325d03fc4b19d5f
Kirsty Burgoine <info@kirstyburgoine.co.uk>
2016-09-10 12:12:38 +0100

    404 Updates

--- wp-content/themes/glowline/404.php
+++ wp-content/themes/glowline/404.php
@@ -5,19 +5,22 @@
 */
 
 get_header(); ?>
-<div class="container" class="clearfix">
-	<div class="page-title breadcrumb">
-		<h1><?php _e( 'Not Found', 'glowline' ); ?></h1>
-	</div>
 </div>
-<div id="page" class="clearfix right" >
-	<div class="content">
+<div id="page" class="clearfix">
+<div class="content"><!-- Content Start -->
+<div class="page-content"><!-- blog-single -->
+	<div class="post-title"><h1><?php _e( 'Not Found', 'glowline' ); ?></h1></div>
+	<div class="page-description">
 		<p><?php _e( 'It looks like nothing was found at this location. Maybe try a search?', 'glowline' ); ?></p>
 		<?php get_search_form(); ?>
 	</div>
-	<div class="sidebar-wrapper">
-		<?php get_sidebar(); ?>
-	</div>
 </div>
-<!-- page End -->
+</div>
+<!-- / Content End -->
+
+<div class="sidebar-wrapper">
+<?php get_sidebar(); ?>
+</div>
+
+</div>
 <?php get_footer(); ?>
