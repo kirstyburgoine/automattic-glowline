00af551a9ceae91ef9365f8e3bbbb96cbb9016e6
Kirsty Burgoine <info@kirstyburgoine.co.uk>
2017-07-17 22:31:31 +0100

    Added title support and removed hardcoded

--- wp-content/themes/glowline/functions.php
+++ wp-content/themes/glowline/functions.php
@@ -56,6 +56,14 @@ function glowline_setup() {
 	add_theme_support( 'automatic-feed-links' );
 
 	/*
+	 * Let WordPress manage the document title.
+	 * By adding theme support, we declare that this theme does not use a
+	 * hard-coded <title> tag in the document head, and expect WordPress to
+	 * provide it for us.
+	 */
+	add_theme_support( 'title-tag' );
+
+	/*
 	 * Enable support for Post Formats.
 	 */
 
@@ -67,6 +75,7 @@ function glowline_setup() {
 	add_theme_support( 'html5', array(
 		'search-form', 'comment-form', 'comment-list', 'gallery', 'caption'
 	) );
+
 
 	/*
 	 * Enable global print stylesheet.
