fae413ca76ba31c8350b6a76b93b40d1a00a3d9e
Kirsty Burgoine <info@kirstyburgoine.co.uk>
2017-07-19 20:39:03 +0100

    moved $themecolors into function

--- wp-content/themes/glowline/inc/wpcom.php
+++ wp-content/themes/glowline/inc/wpcom.php
@@ -8,4 +8,19 @@ $themecolors = array(
 'url' => 'bdb76b',
 );
 }
-?>
+
+
+function glowline_wpcom_setup() {
+	global $themecolors;
+	// Set theme colors for third party services.
+	if ( ! isset( $themecolors ) ) {
+		$themecolors = array(
+			'bg'     => '',
+			'border' => '',
+			'text'   => '',
+			'link'   => '',
+			'url'    => '',
+		);
+	}
+}
+add_action( 'after_setup_theme', 'glowline_wpcom_setup' );
