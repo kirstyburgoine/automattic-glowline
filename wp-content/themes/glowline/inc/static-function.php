0b41e1fa10208f634d41abedea46a5bfa924af12
Kirsty Burgoine <info@kirstyburgoine.co.uk>
2017-07-23 14:55:48 +0100

    Tidied static-functions.php

--- wp-content/themes/glowline/inc/static-function.php
+++ wp-content/themes/glowline/inc/static-function.php
@@ -5,68 +5,74 @@
  * @param
  * @return mixed|string
  */
+
+// ----------------------------------------------------------------------
+// ----------------------------------------------------------------------
+// Main grid layout function
+// ----------------------------------------------------------------------
+// ----------------------------------------------------------------------
 function glowline_grid_thumb($glowline_grid_layout, $thumb_crop=true){
-		if($thumb_crop):
-switch($glowline_grid_layout){
-				case 'two-grid-layout':
-						 the_post_thumbnail('glowline-custom-two-grid-thumb');
-						break;
-				case 'three-grid-layout':
-						 the_post_thumbnail('glowline-custom-three-grid-thumb');
-						break;
-				case 'four-grid-layout':
-						 the_post_thumbnail('glowline-custom-four-grid-thumb');
-						break;
-				case 'five-grid-layout':
-						 the_post_thumbnail('glowline-custom-five-grid-thumb');
-						break;
-				case 'list-layout':
-						 the_post_thumbnail('glowline-custom-list-thumb');
-						break;
-				case 'boxed-layout':
-						 the_post_thumbnail('glowline-custom-boxed-thumb');
-						break;
-}
-endif;
+	if ( $thumb_crop ) :
+		switch ( $glowline_grid_layout ) {
+			case 'two-grid-layout':
+				the_post_thumbnail('glowline-custom-two-grid-thumb');
+				break;
 
-}
+			case 'three-grid-layout':
+				the_post_thumbnail('glowline-custom-three-grid-thumb');
+				break;
 
-if ( ! function_exists( 'glowline_the_custom_logo' ) ) :
-/**
- * Displays the optional custom logo.
- *
- * Does nothing if the custom logo is not available.
- *
- */
-function glowline_the_custom_logo() {
-		if ( function_exists( 'the_custom_logo' ) ) {
-				the_custom_logo();
+			case 'four-grid-layout':
+				the_post_thumbnail('glowline-custom-four-grid-thumb');
+				break;
+
+			case 'five-grid-layout':
+				the_post_thumbnail('glowline-custom-five-grid-thumb');
+				break;
+
+			case 'list-layout':
+				the_post_thumbnail('glowline-custom-list-thumb');
+				break;
+
+			case 'boxed-layout':
+				the_post_thumbnail('glowline-custom-boxed-thumb');
+				break;
 		}
+	endif;
 }
+
+
+
+// ----------------------------------------------------------------------
+// Optional Custom Logo
+// ----------------------------------------------------------------------
+if ( ! function_exists( 'glowline_the_custom_logo' ) ) :
+	/**
+	 * Does nothing if the custom logo is not available.
+	 */
+	function glowline_the_custom_logo() {
+		if ( function_exists( 'the_custom_logo' ) ) {
+			the_custom_logo();
+		}
+	}
 endif;
 
 
 
-/*
- * Custom header menu
- *
-*/
-
+// ----------------------------------------------------------------------
+// Custom header menu
+// ----------------------------------------------------------------------
 add_action( 'after_setup_theme', 'glowline_register_theme_menu' );
+
 function glowline_register_theme_menu() {
 	register_nav_menu( 'primary', __( 'Primary Menu', 'glowline' ) );
 }
 
-function glowline_wp_page_menu() {
-	echo '<ul class="menu" id="menu-2">';
-	wp_list_pages(array('title_li' => ''));
-	echo '</ul>';
-}
 
 
-/**
- * custom post excerpt
- */
+// ----------------------------------------------------------------------
+// custom post excerpt
+// ----------------------------------------------------------------------
 function glowline_custom_excerpt( $length ) {
     return 20;
 }
@@ -74,35 +80,37 @@ add_filter( 'excerpt_length', 'glowline_custom_excerpt', 99 );
 
 
 
+// ----------------------------------------------------------------------
 // related post
-	function glowline_get_related_single_post() {
-		global $post;
-		 $args = array(
-							 'category__in' => wp_get_post_categories($post->ID),
-							 'post__not_in' => array($post->ID),
-								'post_status' => array('publish'),
-								'meta_key' => '_thumbnail_id',
-								'posts_per_page' => 3,
-						);
-				$my_query = new WP_Query($args);
-				if ($my_query->have_posts()) {
-						while ($my_query->have_posts()) : $my_query->the_post();
-								?>
-							 <li class="sl-related-thumbnail">
-									<div class="sl-related-thumbnail-size">
-										<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('glowline-custom-three-grid-thumb',array('class' => "postimg listing-thumbnail")); ?></a>
-									</div>
-									<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-								</li>
-				<?php
-						endwhile;
-				}
-		wp_reset_query(); // to use the original query again
+// ----------------------------------------------------------------------
+function glowline_get_related_single_post() {
+	global $post;
+	$args = array(
+		'category__in' => wp_get_post_categories($post->ID),
+		'post__not_in' => array($post->ID),
+		'post_status' => array('publish'),
+		'meta_key' => '_thumbnail_id',
+		'posts_per_page' => 3,
+	);
+	$my_query = new WP_Query($args);
+		if ($my_query->have_posts()) {
+			while ($my_query->have_posts()) : $my_query->the_post(); ?>
+				<li class="sl-related-thumbnail">
+					<div class="sl-related-thumbnail-size">
+						<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('glowline-custom-three-grid-thumb',array('class' => "postimg listing-thumbnail")); ?></a>
+					</div>
+					<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+				</li>
+		<?php endwhile;
+		}
+	wp_reset_query(); // to use the original query again
 }
 
 
 
-/*hexa to rgba convert*/
+// ----------------------------------------------------------------------
+// hexa to rgba convert
+// ----------------------------------------------------------------------
 function glowline_hex2rgba($color, $opacity = false) {
 
 	$default = 'rgb(0,0,0)';
@@ -145,20 +153,28 @@ function glowline_hex2rgba($color, $opacity = false) {
 
 
 
-/*Number of comment*/
+// ----------------------------------------------------------------------
+// Number of comments
+// ----------------------------------------------------------------------
 function glowline_comment_number(){ ?>
 <span class="post-comment"><?php comments_popup_link(__('Leave a Comment','glowline'), __('1 Comment','glowline'), __('% Comments','glowline')); ?></span>
 <?php }
 
+
+
+// ----------------------------------------------------------------------
+// Specific styling for  Author card
+// ----------------------------------------------------------------------
 function glowline_userpic(){
-		 $address = get_the_author_meta('user_email');
-		 $nicname = get_the_author_meta('user_nicename');
-		$pic = get_avatar( $address, 30, '', $nicname);
-		return $pic;
-	}
+	$address = get_the_author_meta('user_email');
+	$nicname = get_the_author_meta('user_nicename');
+	$pic = get_avatar( $address, 30, '', $nicname);
+	return $pic;
+}
 
 function glowline_share_text(){ ?>
- <ul class="single-social-icon">
-				<li><span class="post-author-pic"> <?php echo glowline_userpic(); ?></span></li>
-				<li><span class="post-author"><?php the_author_posts_link(); ?></span></li>
-				</ul> <?php } ?>
+	<ul class="single-social-icon">
+		<li><span class="post-author-pic"> <?php echo glowline_userpic(); ?></span></li>
+		<li><span class="post-author"><?php the_author_posts_link(); ?></span></li>
+	</ul>
+<?php } ?>
