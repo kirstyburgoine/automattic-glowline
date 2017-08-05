a8c106102943b4448d6769fb154f38b5445c9f2e
Kirsty Burgoine <info@kirstyburgoine.co.uk>
2017-08-05 15:57:46 +0100

    refactored content-grid.php to match content.php

--- wp-content/themes/glowline/partials/content-grid.php
+++ wp-content/themes/glowline/partials/content-grid.php
@@ -5,45 +5,53 @@
 */
 ?>
 <?php global $glowline_grid_layout; ?>
+
 <li id="post-<?php the_ID(); ?>" <?php post_class(); ?> >
-	<div class="post-img">
-		<a href="<?php the_permalink(); ?>"><?php glowline_grid_thumb($glowline_grid_layout); ?></a>
-	</div>
+
+	<?php if ( has_post_thumbnail() ) : ?>
+		<div class="post-img">
+			<a href="<?php the_permalink(); ?>"><?php glowline_grid_thumb($glowline_grid_layout); ?></a>
+		</div>
+	<?php endif; ?>
+
+
 	<div class="post-content">
+
 		<div class="post-content-inner">
-			<div class="post-header">
+
+			<header class="post-header">
+
 				<span class="post-category">
 					<?php echo $category_list = get_the_category_list( __( ', ', 'glowline' ) ); ?>
 				</span>
 
-				<div class="post-title">
-					<a href="<?php the_permalink(); ?>">
-						<?php the_title('<h2>', '</h2>'); ?>
-					</a>
-				</div>
-				<div class="post-meta">
-					<span class="post-date"><?php the_time('M d, Y'); ?></span>
+				<?php the_title( '<div class="post-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark"><h2 class="entry-title">', '</h2></a></div>' ); ?>
 
-					<?php glowline_comment_number(); ?>
 
-				</div>
-			</div>
-			<div class="description"><p><?php the_content( sprintf(
-				wp_kses(
-					/* translators: %s: Name of current post. Only visible to screen readers */
-					__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', '_s' ),
-					array(
-						'span' => array(
-							'class' => array(),
-						),
-					)
-				),
-				get_the_title()
-			) );
-			 ?></p></div>
-			<div class="read-more">
-				<a href="<?php the_permalink(); ?>"><?php _e('Continue Reading...','glowline'); ?></a>
-			</div>
-		</div>
-	</div>
+				<?php glowline_posted_on(); ?>
+
+				<?php glowline_comment_number(); ?>
+
+			</header>
+
+			<div class="description">
+			<?php
+				the_content( sprintf(
+					wp_kses(
+						/* translators: %s: Name of current post. Only visible to screen readers */
+						__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', '_s' ),
+						array(
+							'span' => array(
+								'class' => array('read-more'),
+							),
+						)
+					),
+					get_the_title()
+				) );
+			?>
+			</div> <!-- .description //-->
+
+		</div> <!-- .post-content-inner //-->
+	</div> <!-- .post-content //-->
+
 </li>
