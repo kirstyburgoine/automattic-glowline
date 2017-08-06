533dc970b6df82b088911410d1b32be2956ea9fc
Kirsty Burgoine <info@kirstyburgoine.co.uk>
2017-08-06 13:15:40 +0100

    navigation tweaks

--- wp-content/themes/glowline/header.php
+++ wp-content/themes/glowline/header.php
@@ -67,15 +67,16 @@
 
 				<?php glowline_the_custom_logo(); ?>
 
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+				<h1 class="site-title"><?php esc_html( bloginfo( 'name' ) ); ?></h1>
 
 				<?php
 				$description = get_bloginfo( 'description', 'display' );
 
 				if ( $description || is_customize_preview() ) : ?>
-					<p class="site-description"><?php echo $description; ?></p>
+					<p class="site-description"><?php echo esc_html( $description ); ?></p>
 				<?php
 				endif;  ?>
+
 			</div>
 			<!-- Ends Main Logo -->
 
