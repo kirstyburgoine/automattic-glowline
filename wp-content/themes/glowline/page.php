fbe2c7edf0a145a49299349c72cfd4ced3e1785e
Kirsty Burgoine <info@kirstyburgoine.co.uk>
2017-08-13 10:46:36 +0100

    formatted single CSS and tidied page.php

--- wp-content/themes/glowline/page.php
+++ wp-content/themes/glowline/page.php
@@ -3,45 +3,32 @@
 * @package Glowline
 * The template for displaying all pages
 */
-get_header();
 
-?>
-</div>
-<div id="page" class="clearfix">
-<div class="content"><!-- Content Start -->
-<div class="page-content"><!-- blog-single -->
-<?php if (have_posts()) : while (have_posts()) : the_post();?>
-<?php the_title('<div class="post-title"><h1>', '</h1></div>'); ?>
-<div class="page-description">
-	<?php the_content(); ?>
-	<?php edit_post_link( __( 'Edit', 'glowline' ), '<span class="edit-link">', '</span>' ); ?>
-</div>
-	<div class="multipage-links">
+get_header(); ?>
+
+<main id="main" class="site-main">
+
+	<div class="container clearfix">
+
 		<?php
-			wp_link_pages( array(
-						'before'      => '<span class="meta-nav">' . __( 'Pages:', 'glowline' ) . '</span>',
-						'after'       => '',
-						'link_before' => '<span class="active">',
-						'link_after'  => '</span>',
-					) );
-		?>
-	</div>
-	<div class="clearfix"></div>
-<?php
-// If comments are open or we have at least one comment, load up the comment template.
-if ( comments_open() || get_comments_number() ) {
-comments_template();
-}
-endwhile;
-endif;
-?>
-</div>
-</div>
-<!-- / Content End -->
+		if (have_posts()) :
+			while (have_posts()) : the_post();
 
-<div class="sidebar-wrapper">
-<?php get_sidebar(); ?>
-</div>
+				get_template_part( 'partials/content', 'post' );
 
-</div>
+				// If comments are open or we have at least one comment, load up the comment template.
+				if ( comments_open() || get_comments_number() ) :
+					comments_template();
+				endif;
+
+			endwhile;
+		endif;
+
+		get_sidebar();
+	?>
+
+	</div><!-- .container //-->
+
+</main>
+
 <?php get_footer(); ?>
