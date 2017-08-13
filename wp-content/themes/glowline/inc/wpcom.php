fe30d5037436244b5983bbd3e8fdf5a08153d8ff
Kirsty Burgoine <info@kirstyburgoine.co.uk>
2017-08-13 15:03:27 +0100

    Updated theme colours settings

--- wp-content/themes/glowline/inc/wpcom.php
+++ wp-content/themes/glowline/inc/wpcom.php
@@ -1,15 +1,4 @@
 <?php
-if ( ! isset( $themecolors ) ) {
-$themecolors = array(
-'bg' => 'ffffff',
-'text' => '424242',
-'link' => 'bdb76b',
-'border' => 'dddddd',
-'url' => 'bdb76b',
-);
-}
-
-
 function glowline_wpcom_setup() {
 	global $themecolors;
 	// Set theme colors for third party services.
