1ab3e2a29a0d1b255fe29942be24e90b7d872986
Kirsty Burgoine <info@kirstyburgoine.co.uk>
2017-08-21 19:59:41 +0100

    removed duplicated site title

--- wp-content/themes/glowline/header.php
+++ wp-content/themes/glowline/header.php
@@ -26,13 +26,10 @@
 			<div class="container header">
 					<!-- Logo Start -->
 					<div class="logo">
+
 						<?php glowline_the_custom_logo(); ?>
-						<?php
-						if ( is_front_page() && is_home() ) : ?>
-							<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-						<?php else : ?>
-							<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-						<?php endif; ?>
+						<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+
 					</div>
 					<!-- Menu Start -->
 					<div id="main-menu-wrapper">
