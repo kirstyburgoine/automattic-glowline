e87b858766b70189b87635337927fd9ffc767b17
Kirsty Burgoine <info@kirstyburgoine.co.uk>
2017-09-02 12:21:59 +0100

    removed unneeded stylesheets

--- wp-content/themes/glowline/functions.php
+++ wp-content/themes/glowline/functions.php
@@ -104,7 +104,6 @@ function glowline_scripts() {
 
 	wp_enqueue_style( 'glowline-fonts', glowline_fonts_url(), array(), null );
 	wp_enqueue_style( 'glowline-font-awesome', get_template_directory_uri() . '/css/font-awesome.css', array(), '1.0.0' );
-	wp_enqueue_style( 'glowline-widgets', get_template_directory_uri() . '/css/widget-styles.css', array(), '1.0.0' );
 	// Load our main stylesheet.
 	wp_enqueue_style( 'glowline-style', get_stylesheet_uri() );
 
