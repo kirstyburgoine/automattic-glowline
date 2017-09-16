e87f231f19dbcef9fb4c3da0e66ffc980680f3c4
Kirsty Burgoine <info@kirstyburgoine.co.uk>
2017-09-16 10:10:44 +0100

    Changed site-title to be h1 or p tag
    also updated CSS so that styles looking for class not h1

--- wp-content/themes/glowline/header.php
+++ wp-content/themes/glowline/header.php
@@ -37,7 +37,15 @@
 							the_custom_logo();
 						}
 						?>
-						<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+
+						<?php
+						if ( is_front_page() && is_home() ) : ?>
+							<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+						<?php else : ?>
+							<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+						<?php
+						endif;
+						?>
 
 					</div>
 					<div id="main-menu-wrapper">
