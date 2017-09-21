825bb7909ab64dbc1ed9231938cbb8420987e34f
Kirsty Burgoine <info@kirstyburgoine.co.uk>
2017-09-21 19:20:34 +0100

    Removed additional markup in sidebar

--- wp-content/themes/glowline/sidebar.php
+++ wp-content/themes/glowline/sidebar.php
@@ -12,9 +12,7 @@ if ( is_active_sidebar( 'sidebar-1' ) ) :
 
 	<div class="sidebar-wrapper"><!-- left -->
 		<aside class="sidebar">
-			<div class="widget">
 				<?php dynamic_sidebar( 'sidebar-1' ); ?>
-			</div>
 		</aside>
 	</div>
 
