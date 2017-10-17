eea4e0b6645c422653eb4f2d5b9ad2a3a9f8ad4c
Kirsty Burgoine <info@kirstyburgoine.co.uk>
2017-10-17 19:35:21 +0100

    Changed carousel text to fullwidth if no image

--- wp-content/themes/glowline/templates/main-slider.php
+++ wp-content/themes/glowline/templates/main-slider.php
@@ -21,29 +21,52 @@ foreach ( $featured_posts as $post ) :
 			<a href="<?php esc_url( the_permalink() ); ?>">
 				<?php the_post_thumbnail( 'glowline-custom-slider-thumb' ); ?>
 			</a>
+
+			<div class="slider-post-content-wrapper">
+				<div class="slider-post-content">
+
+					<?php glowline_posted_in(); ?>
+
+					<?php the_title( '<div class="slider-post-title"><h2><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2></div>' ); ?>
+
+					<?php glowline_posted_on(); ?>
+
+					<?php the_excerpt(); ?>
+
+					<div class="read-more read-more-slider">
+						<a href="<?php esc_url( the_permalink() ); ?>">
+							<?php echo esc_html( 'Continue Reading...', 'glowline' ); ?>
+						</a>
+					</div>
+
+				</div>
+			</div>
+
+		<?php
+		else :
+		?>
+			<div class="slider-post-content-wrapper">
+				<div class="slider-post-content no-image">
+
+					<?php glowline_posted_in(); ?>
+
+					<?php the_title( '<div class="slider-post-title"><h2><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2></div>' ); ?>
+
+					<?php glowline_posted_on(); ?>
+
+					<?php the_content(); ?>
+
+					<div class="read-more read-more-slider">
+						<a href="<?php esc_url( the_permalink() ); ?>">
+							<?php echo esc_html( 'Continue Reading...', 'glowline' ); ?>
+						</a>
+					</div>
+
+				</div>
+			</div>
 		<?php
 		endif;
 		?>
-
-		<div class="slider-post-content-wrapper">
-			<div class="slider-post-content">
-
-				<?php glowline_posted_in(); ?>
-
-				<?php the_title( '<div class="slider-post-title"><h2><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2></div>' ); ?>
-
-				<?php glowline_posted_on(); ?>
-
-				<?php the_excerpt(); ?>
-
-				<div class="read-more read-more-slider">
-					<a href="<?php esc_url( the_permalink() ); ?>">
-						<?php echo esc_html( 'Continue Reading...', 'glowline' ); ?>
-					</a>
-				</div>
-
-			</div>
-		</div>
 
 	</div>
 
