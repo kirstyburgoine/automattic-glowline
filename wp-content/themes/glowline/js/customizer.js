a005ea6e6a24c80a90ee8f01b6ee24e2f6f096a4
Kirsty Burgoine <info@kirstyburgoine.co.uk>
2017-10-26 21:16:16 +0100

    formatted customizer.js - phpbcf

--- wp-content/themes/glowline/js/customizer.js
+++ wp-content/themes/glowline/js/customizer.js
@@ -10,31 +10,43 @@
 ( function( $ ) {
 
 	// Site title and description.
-	wp.customize( 'blogname', function( value ) {
-		value.bind( function( to ) {
-			$( '.site-title span' ).text( to );
-		} );
-	} );
+	wp.customize(
+		'blogname', function( value ) {
+			value.bind(
+				function( to ) {
+					$( '.site-title span' ).text( to );
+				}
+			);
+		}
+	);
 
-	wp.customize( 'blogdescription', function( value ) {
-		value.bind( function( to ) {
-			$( '.site-description' ).text( to );
-		} );
-	} );
+	wp.customize(
+		'blogdescription', function( value ) {
+			value.bind(
+				function( to ) {
+					$( '.site-description' ).text( to );
+				}
+			);
+		}
+	);
 
 	// Grid Settings.
-	wp.customize( 'dynamicgrid', function( value ) {
-		value.bind( function( to ) {
-			if ( 'standard-layout' === to ) {
-				$( '#content' ).addClass( 'standard-layout' ).removeClass( 'masonry-enabled two-grid-layout three-grid-layout four-grid-layout' );
-			} else if( 'two-grid-layout' === to ) {
-				$( '#content' ).addClass( 'masonry-enabled two-grid-layout' ).removeClass( 'standard-layout three-grid-layout four-grid-layout' );
-			} else if( 'three-grid-layout' === to ) {
-				$( '#content' ).addClass( 'masonry-enabled three-grid-layout' ).removeClass( 'standard-layout two-grid-layout four-grid-layout' );
-			} else {
-				$( '#content' ).addClass( 'masonry-enabled four-grid-layout' ).removeClass( 'standard-layout two-grid-layout three-grid-layout' );
-			}
-		} );
-	} );
+	wp.customize(
+		'dynamicgrid', function( value ) {
+			value.bind(
+				function( to ) {
+					if ( 'standard-layout' === to ) {
+						$( '#content' ).addClass( 'standard-layout' ).removeClass( 'masonry-enabled two-grid-layout three-grid-layout four-grid-layout' );
+					} else if ( 'two-grid-layout' === to ) {
+						$( '#content' ).addClass( 'masonry-enabled two-grid-layout' ).removeClass( 'standard-layout three-grid-layout four-grid-layout' );
+					} else if ( 'three-grid-layout' === to ) {
+						$( '#content' ).addClass( 'masonry-enabled three-grid-layout' ).removeClass( 'standard-layout two-grid-layout four-grid-layout' );
+					} else {
+						$( '#content' ).addClass( 'masonry-enabled four-grid-layout' ).removeClass( 'standard-layout two-grid-layout three-grid-layout' );
+					}
+				}
+			);
+		}
+	);
 
 } )( jQuery );
